f4ead73b0d543896c7ad943cdfa133804fe7202b
nuyada <vova-isaev-2015@mail.ru>
2024-07-17 13:37:57 +0300

    Добавьте файлы проекта.

---
+++ Practicum9.6.1/MyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum9._6._1
+{
+    internal class MyException: Exception
+    {
+        public MyException(string message): base(message)
+        {
+
+        }
+    }
+}
---
+++ Practicum9.6.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum9._6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions =
+            {
+                new DivideByZeroException(),
+                new IndexOutOfRangeException(),
+                new FormatException(),
+                new MyException("Это мое собственное исключение"),
+                new NullReferenceException()
+            };
+            try
+            {
+                foreach(var exception in exceptions)
+                {
+                    throw exception;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Возникло исключение" + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally выполнен.");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
